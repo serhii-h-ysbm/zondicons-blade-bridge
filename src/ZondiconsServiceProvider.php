0bbae1b44a34c28ce6ec9702a81f4e8e6b65d735
bengs <20150565+bengs@users.noreply.github.com>
2019-08-14 11:03:09 +0100

    Update ZondiconsServiceProvider.php

--- src/ZondiconsServiceProvider.php
+++ src/ZondiconsServiceProvider.php
@@ -2,15 +2,15 @@
 
 namespace Zondicons;
 
-use BladeSvg\IconFactory;
-use Illuminate\Support\Facades\Blade;
+use BladeSvg\SvgFactory;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class ZondiconsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        app(IconFactory::class)->registerBladeTag();
+        app(SvgFactory::class)->registerBladeTag();
 
         $this->publishes([
             __DIR__.'/../config/zondicons.php' => config_path('zondicons.php'),
@@ -19,14 +19,13 @@ class ZondiconsServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(IconFactory::class, function () {
-            $config = array_merge([
-                'icon_path' => base_path('vendor/zondicons/blade-bridge/resources/icons'),
+        $this->app->singleton(SvgFactory::class, function () {
+            $config = Collection::make(config('blade-svg', []))->merge([
                 'spritesheet_path' => base_path('vendor/zondicons/blade-bridge/resources/sprite.svg'),
+                'svg_path' => base_path('vendor/zondicons/blade-bridge/resources/icons'),
                 'sprite_prefix' => 'zondicon-',
-            ], config('zondicons', []));
-
-            return new IconFactory($config);
+            ])->all();
+            return new SvgFactory($config);
         });
     }
 }
